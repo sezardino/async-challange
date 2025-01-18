8a74f69190a68200a4b20f81159f06b5661f8aa8
Edward <e.arechwa@gmail.com>
2025-01-18 23:25:52 +0100

    refactor: cleanup solution three

--- src/components/solutions/level-three/solution.tsx
+++ src/components/solutions/level-three/solution.tsx
@@ -12,8 +12,9 @@ import {
 } from "@/components/ui/tooltip";
 import { MAX_PARALLEL_REQUESTS_COUNT } from "@/const/requests";
 import { useSaveMutation } from "@/hooks/mutations/save";
+import { useRequestsLimiter } from "@/hooks/requests-limiter";
 import { cn } from "@/lib/utils";
-import { ProcessedRequest, SortType } from "@/types";
+import { SortType } from "@/types";
 import { Loader } from "lucide-react";
 import { ComponentPropsWithoutRef, useMemo, useState } from "react";
 import { Button } from "../../ui/button";
@@ -24,46 +25,14 @@ export const LevelThreeSolution = (props: LevelThreeSolutionProps) => {
   const { className, ...rest } = props;
 
   const [sortBy, setSortBy] = useState<SortType | null>(null);
-  const [requests, setRequests] = useState<ProcessedRequest[]>([]);
 
   const { mutateAsync: save } = useSaveMutation();
 
-  const updateRequest = (request: Omit<ProcessedRequest, "createdAt">) =>
-    setRequests((prev) =>
-      prev.map((req) => (req.id !== request.id ? req : { ...req, ...request }))
-    );
-
-  const saveHandler = async () => {
-    if (pendingRequestsCount >= MAX_PARALLEL_REQUESTS_COUNT) return;
-
-    const newRequest: ProcessedRequest = {
-      id: crypto.randomUUID(),
-      status: "pending",
-      createdAt: Date.now(),
-    };
-
-    try {
-      setRequests((prev) => [...prev, newRequest]);
-      const res = await save();
-
-      updateRequest({
-        id: newRequest.id,
-        status: "success",
-        message: res,
-      });
-    } catch (error) {
-      const message =
-        typeof error === "string"
-          ? error
-          : "Error: something went wrong, try again";
-
-      updateRequest({
-        id: newRequest.id,
-        status: "error",
-        message,
-      });
-    }
-  };
+  const {
+    requests,
+    pendingCount: pendingRequestsCount,
+    saveRequest: saveHandler,
+  } = useRequestsLimiter(save);
 
   const sortChange = () => {
     setSortBy((prev) => {
@@ -79,11 +48,6 @@ export const LevelThreeSolution = (props: LevelThreeSolutionProps) => {
       sortBy === "asc" ? a.createdAt - b.createdAt : b.createdAt - a.createdAt
     );
   }, [requests, sortBy]);
-
-  const pendingRequestsCount = useMemo(
-    () => requests.filter((r) => r.status === "pending").length,
-    [requests]
-  );
 
   const requestLimitExceeded =
     pendingRequestsCount >= MAX_PARALLEL_REQUESTS_COUNT;
