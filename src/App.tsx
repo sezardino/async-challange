e7334bf5b9fedacd88f4e44c013cd21cb76eb9d8
Edward <e.arechwa@gmail.com>
2025-01-18 19:43:27 +0100

    feat: initial statement

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
+import { faker } from "@faker-js/faker";
+
+const saveFile = () =>
+  new Promise((res, rej) => {
+    const timeToResolve = faker.number.int({ min: 1000, max: 3000 });
+    const isSuccess = Math.random() > 0.5;
+    const message = isSuccess
+      ? `Success: ${faker.system.commonFileName()} saved`
+      : `Error: ${faker.system.commonFileName()} not saved`;
+
+    setTimeout(() => {
+      if (isSuccess) res(message);
+      else rej(message);
+    }, timeToResolve);
+  });
+
 function App() {
   return (
-    <>
-      <h1 className="text-xl text-red-500">Async challenge</h1>
-    </>
+    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-screen w-screen">
+      <h1>Async Challange</h1>
+      <button className="bg-blue-400" onClick={() => saveFile()}>
+        Save File
+      </button>
+    </div>
   );
 }
 
